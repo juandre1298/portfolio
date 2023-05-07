7c9c3e04e279b2728901761462115bc9c5dd1296
juandre1298 <juandre1298@gmail.com>
2023-05-07 11:23:59 -0500

    nothing

--- src/pages/detail/[id].js
+++ src/pages/detail/[id].js
@@ -1,7 +1,6 @@
 import React from "react";
-
+// nothing
 import Link from "next/link";
-
 import { useState } from "react";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
